e90f51bcc90fd114903db933dd2f12960f0ee345
Alina Bregvadze <alina.bregvadze@gmail.com>
2024-10-22 17:22:44 +0400

    Updates

--- assets/BackToMain-CqipoAvy.js
+++ assets/BackToMain-CqipoAvy.js
@@ -1,4 +1,4 @@
-import{r as ge,g as K_,j as yn,c as dM,L as pM}from"./index-BjbbLgGF.js";/**
+import{r as ge,g as K_,j as yn,c as dM,L as pM}from"./index-DSdV0nTt.js";/**
  * @license
  * Copyright 2010-2024 Three.js Authors
  * SPDX-License-Identifier: MIT
