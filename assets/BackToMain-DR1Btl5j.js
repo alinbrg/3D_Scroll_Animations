babd3d71a571f36884299a0ae15b62537ee936eb
Alina Bregvadze <alina.bregvadze@gmail.com>
2024-10-22 17:16:36 +0400

    Updates

--- assets/BackToMain-DR1Btl5j.js
+++ assets/BackToMain-DR1Btl5j.js
@@ -1,4 +1,4 @@
-import{r as ge,g as K_,j as yn,c as dM,L as pM}from"./index-D2V-gVjO.js";/**
+import{r as ge,g as K_,j as yn,c as dM,L as pM}from"./index-BjbbLgGF.js";/**
  * @license
  * Copyright 2010-2024 Three.js Authors
  * SPDX-License-Identifier: MIT
